aedb4f1f57c95994d0a18ea6586b447404c439b4
heler <ngochoang0511.1995@gmail.com>
2020-11-05 16:18:27 +0700

    new code

---
+++ header/header.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>  
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Minh Đức Điện Công Nghiệp Tự Động Hóa Quảng Nam</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Knight - v2.1.0
+  * Template URL: https://bootstrapmade.com/knight-free-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+<body>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+     <img src="assets/img/logomd.png" alt="">
+      <h1 data-aos="zoom-in">WELCOME TO US</h1>
+      <h2 data-aos="fade-up">CHÚNG TÔI LÀ CHUYÊN TƯ VẤN, SỬA CHỮA, THIẾT KẾ, CÁC VẤN ĐỀ LIÊN QUAN ĐẾN ĐIỆN CÔNG NGHIỆP VÀ TỰ ĐỘNG HÓA</h2>
+      <a data-aos="fade-up" href="#about" class="btn-get-started scrollto">Về Chúng Tôi</a>
+    </div>
+  </section><!-- End Hero -->
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container">
+
+      <!-- The main logo is shown in mobile version only. The centered nav-logo in nav menu is displayed in desktop view  -->
+      <div class="logo d-block d-lg-none">
+        <a href="index.html"><img src="assets/img/logomd.png" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul class="nav-inner">
+          <li class="active"><a href="index.html">Trang Chủ</a></li>
+          <li class="drop-down"><a href="">Về Chúng Tôi</a>
+            <ul>
+              <li><a href="#about">Chúng Tôi</a></li>
+              <li><a href="#team">Nhóm</a></li>
+
+             <!--  <li class="drop-down"><a href="#">Deep Drop</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li> -->
+            </ul>
+          </li>
+          <li><a href="#services">Dịch Vụ</a></li>
+
+          <li class="nav-logo"><a href="index.html"><img src="assets/img/logomd.png" alt="" class="img-fluid"></a></li>
+
+          <li><a href="#portfolio">Nhà Đầu Tư</a></li>
+         <!--  <li><a href="#pricing">Pricing</a></li> -->
+          <li><a href="#contact">Thông Tin Liên Hệ</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+	include("header/header.php");
+	include("content/content.php");
+	include("footer/footer.php");
+?>
